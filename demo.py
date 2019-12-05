60a8ecd0389dc3d733a0d0c82e35cc63096254b6
Loftie Ellis <lpellis@gmail.com>
2019-12-05 12:15:35 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ import sys
 import os
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
-if os.path.exists(libdir):
+if os.path.exists(libdir): #
     sys.path.append(libdir)
 
 import logging
